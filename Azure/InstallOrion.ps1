e2dec8adfe11aba7a1a9a5097ac86c03688fdeb4
Ladinek Vargu <Ladislav.Varga@solarwinds.com>
2019-06-14 23:00:53 +0200

    add silent config

--- Azure/InstallOrion.ps1
+++ Azure/InstallOrion.ps1
@@ -1,5 +1,7 @@
 $url = "https://downloads.solarwinds.com/solarwinds/OnlineInstallers/RTM/NPM/Solarwinds-Orion-NPM.exe"
+$urlSilentConfig = "https://github.com/LadaVarga/cloudTemplates/blob/master/Azure/silentconfig.xml"
 $output = "c:\temp\Solarwinds-Orion-NPM.exe"
+$outputsilent = "C:\temp\silentconfig.xml"
 $start_time = Get-Date
 
 $wc = New-Object System.Net.WebClient
@@ -7,4 +9,11 @@ $wc.DownloadFile($url, $output)
 #OR
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($urlSilentConfig ,$outputsilent)
+#OR
+(New-Object System.Net.WebClient).DownloadFile($urlSilentConfig, $outputsilent)
+
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+start /wait c:\temp\Solarwinds-Orion-NPM.exe /s /notests /ConfigFile="C:\temp\silentconfig.xml"
