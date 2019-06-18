4fbaa748ce5353d42b5e18e95f8744b8ff054942
Unknown <Ladislav.Varga@solarwinds.com>
2019-06-18 09:25:38 +0200

    Fixes

--- Azure/InstallOrion.ps1
+++ Azure/InstallOrion.ps1
@@ -1,11 +1,11 @@
 $url = "https://downloads.solarwinds.com/solarwinds/OnlineInstallers/RTM/NPM/Solarwinds-Orion-NPM.exe"
 $urlSilentConfig = "https://raw.githubusercontent.com/LadaVarga/cloudTemplates/master/Azure/silentconfig.xml"
-$output = "c:\temp\Solarwinds-Orion-NPM.exe"
-$outputsilent = "C:\temp\silentconfig.xml"
+$output = "c:\windows\temp\Solarwinds-Orion-NPM.exe"
+$outputsilent = "c:\windows\temp\silentconfig.xml"
 $start_time = Get-Date
 
-New-Item -ItemType "directory" -path c:\temp -force
-set-location c:\temp
+
+Set-location C:\windows\Temp
 
 $wc = (New-Object System.Net.WebClient)
 $wc.DownloadFile($url, $output)
@@ -19,4 +19,4 @@ $wc.DownloadFile($urlSilentConfig ,$outputsilent)
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-start -Wait -FilePath "c:\temp\Solarwinds-Orion-NPM.exe" -ArgumentList "/s /notests /ConfigFile=`"C:\temp\silentconfig.xml`""
+start -Wait -FilePath "c:\windows\temp\Solarwinds-Orion-NPM.exe" -ArgumentList "/s /notests /ConfigFile=`"c:\windows\temp\silentconfig.xml`""
