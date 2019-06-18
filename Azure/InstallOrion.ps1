14b788f3c13f6e7da703edba23f36497e75bbcef
Unknown <Ladislav.Varga@solarwinds.com>
2019-06-18 10:23:52 +0200

    change to eval

--- Azure/InstallOrion.ps1
+++ Azure/InstallOrion.ps1
@@ -1,5 +1,5 @@
 $url = "https://downloads.solarwinds.com/solarwinds/OnlineInstallers/RTM/NPM/Solarwinds-Orion-NPM.exe"
-$urlSilentConfig = "https://raw.githubusercontent.com/LadaVarga/cloudTemplates/master/Azure/silentconfig.xml"
+$urlSilentConfig = "https://raw.githubusercontent.com/LadaVarga/cloudTemplates/master/Azure/silentconfigEval.xml"
 $output = "c:\windows\temp\Solarwinds-Orion-NPM.exe"
 $outputsilent = "c:\windows\temp\silentconfig.xml"
 $start_time = Get-Date
@@ -19,4 +19,4 @@ $wc.DownloadFile($urlSilentConfig ,$outputsilent)
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-start -Wait -FilePath "c:\windows\temp\Solarwinds-Orion-NPM.exe" -ArgumentList "/s /notests /ConfigFile=`"c:\windows\temp\silentconfig.xml`""
+start -Wait -FilePath "c:\windows\temp\Solarwinds-Orion-NPM.exe" -ArgumentList "/s /notests /ConfigFile=`"c:\windows\temp\silentconfigEval.xml`""
