e8beef9dd9c4ee7d91ed7952a182819cc7dca684
Unknown <Ladislav.Varga@solarwinds.com>
2019-06-19 10:55:53 +0200

    update for waiting for installation

--- Azure/InstallOrion-internal.ps1
+++ Azure/InstallOrion-internal.ps1
@@ -25,4 +25,47 @@ $wc.DownloadFile($urlSilentConfig ,$outputsilent)
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-Start-Process -wait -FilePath "c:\windows\temp\Solarwinds-Orion-NPM.exe" -ArgumentList "/s /notests /ConfigFile=`"c:\windows\temp\silentconfig.xml`""
+
+
+
+$executable = "c:\windows\temp\Solarwinds-Orion-NPM.exe"
+$installerparameters = "/s /notests /ConfigFile=`"c:\windows\temp\silentconfig.xml`""
+$installertimeoutsec = 7200
+try{
+
+            write-host "starting orion installer: `"$executable`" `"$installerparameters`""
+            $process = [system.diagnostics.process]::start($executable, $installerparameters)
+            write-host "pid $($process.id) - waiting"
+            wait-process -id ($process.id) -timeout ([int]::parse($installertimeoutsec))
+
+            if (-not $process.hasexited)
+            {
+                write-host "product `"$installername`": installer is still running after time out."
+                throw "product `"$installername`": installer timed out"
+            }
+
+            if (($process.exitcode) -ne 0)
+            {
+                $exitcode = $process.exitcode
+                write-host "product `"$installername`": installer finished with following exit code: $exitcode"
+                throw "product `"$installername`": installer finished with error"
+            }
+
+            $installerstartedevent = (get-param $productconfig "installerstartedscript" "")
+            if ($installerstartedevent) { invoke-expression $installerstartedevent }
+            
+            }
+            catch
+    {
+        # something went wrong
+        # take screenshot
+       Write-host "Installation failed"
+
+        throw $_
+    }
+
+
+
+#Start-Process -wait -FilePath "c:\windows\temp\Solarwinds-Orion-NPM.exe" -ArgumentList "/s /notests /ConfigFile=`"c:\windows\temp\silentconfig.xml`""
+
+#cmd start /wait "c:\windows\temp\Solarwinds-Orion-NPM.exe" /s /ConfigFile="c:\windows\temp\silentconfig.xml"
